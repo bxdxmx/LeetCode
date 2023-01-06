d7022cfa703441563de025045eee906b4a35f4a1
eiji <eijiochiai8@yahoo.co.jp>
2023-01-06 09:25:19 +0900

    20230106

---
+++ 1833. Maximum Ice Cream Bars/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MaxIceCream(int[] costs, int coins)
+    {
+        int res = 0;
+
+        Array.Sort(costs);
+
+        for(int i = 0; i < costs.Length && coins > 0; i++)
+        {
+            if (coins >= costs[i])
+            {
+                coins -= costs[i];
+                res++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ 713. Subarray Product Less Than K/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int NumSubarrayProductLessThanK(int[] nums, int k)
+    {
+        int res = 0;
+
+        if (k <= 1)
+        {
+            return res;
+        }
+
+        int product = 1, left = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            product *= nums[right];
+
+            while (product >= k)
+            {
+                product /= nums[left++];
+            }
+
+            res += right - left + 1;
+        }
+
+        return res;
+    }
+}
