1a17aa3398534ea9b541bf76b5eca7e1421d66ba
eiji <eijiochiai8@yahoo.co.jp>
2023-01-07 13:47:42 +0900

    20230107

---
+++ 134. Gas Station/Program.cs
@@ -0,0 +1,32 @@
+﻿var s = new Solution();
+
+int[] gas = new int[] { 1, 2, 3, 4, 5 };
+int[] cost = new int[] { 3, 4, 5, 1, 2 };
+
+s.CanCompleteCircuit(gas, cost);
+
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        if (gas.Sum() - cost.Sum() < 0)
+        {
+            return -1;
+        }
+
+        int start = 0, remaining = 0;
+
+        for (int i = 0; i < gas.Length; i++)
+        {
+            remaining += gas[i] - cost[i];
+
+            if (remaining < 0)
+            {
+                start = i + 1;
+                remaining = 0;
+            }
+        }
+
+        return start;
+    }
+}
---
+++ 910. Smallest Range II/Program.cs
@@ -0,0 +1,24 @@
+﻿var s = new Solution();
+
+int[] nums = new int[] { 4, 7, 4 };
+int k = 4;
+
+s.SmallestRangeII(nums, k);
+
+public class Solution
+{
+    public int SmallestRangeII(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int ans = nums[^1] - nums[0];
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            int high = Math.Max(nums[^1] - k, nums[i] + k);
+            int low = Math.Min(nums[0] + k, nums[i + 1] - k);
+            ans = Math.Min(ans, high - low);
+        }
+
+        return ans;
+    }
+}
