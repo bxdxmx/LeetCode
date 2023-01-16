401401eec3cfd74b44ea51378e32000c24c1e15d
eiji <eijiochiai8@yahoo.co.jp>
2023-01-16 10:09:26 +0900

    20230116

---
+++ 57. Insert Interval/Program.cs
@@ -0,0 +1,52 @@
+﻿var s = new Solution();
+
+int[][] intervals = new int[][]
+{
+    new int[]{ 1,5},
+};
+
+int[] newInterval = new int[] { 1, 7 };
+
+s.Insert(intervals, newInterval);
+
+public class Solution
+{
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+        List<int[]> res = new();
+        PriorityQueue<(int start, int end), int> q = new();
+        q.Enqueue((newInterval[0], newInterval[1]), newInterval[0]);
+
+        foreach (var interval in intervals)
+        {
+            q.Enqueue((interval[0], interval[1]), interval[0]);
+        }
+
+        while (q.Count > 0)
+        {
+            (int s1, int e1) = q.Dequeue();
+
+            if (q.Count == 0)
+            {
+                res.Add(new int[] { s1, e1 });
+                break;
+            }
+            else
+            {
+                (int s2, int e2) = q.Peek();
+
+                if (s1 <= s2 && s2 <= e1)
+                {
+                    q.Dequeue();
+                    q.Enqueue((s1, Math.Max(e1, e2)), s1);
+                }
+                else
+                {
+                    res.Add(new int[] { s1, e1 });
+                }
+            }
+        }
+
+        return res.ToArray();
+    }
+}
--- 6293. Count the Number of Good Subarrays/Program.cs
+++ 6293. Count the Number of Good Subarrays/Program.cs
@@ -6,59 +6,32 @@ public class Solution
 {
     public long CountGood(int[] nums, int k)
     {
-        Dictionary<int, long> d = new();
-
-        long PairSum(int n)
-        {
-            if (n % 2 == 0)
-            {
-                return (1 + n) * (n / 2);
-            }
-            else
-            {
-                return (1 + n) * (n / 2) + (int)Math.Ceiling(n / 2.0);
-            }
-        }
-
         long res = 0;
         int n = nums.Length;
 
+        long pairsum = 0;
+        int j = 0;
+        Dictionary<int, int> numsum = new();
+
         for (int i = 0; i < n; i++)
         {
-            Dictionary<int, int> numsum = new();
-
-            for (int j = i; j < n; j++)
+            if (numsum.ContainsKey(nums[i]))
             {
-                if (numsum.ContainsKey(nums[j]))
-                {
-                    numsum[nums[j]]++;
-                }
-                else
-                {
-                    numsum[nums[j]] = 1;
-                }
+                numsum[nums[i]]++;
+            }
+            else
+            {
+                numsum[nums[i]] = 1;
             }
 
-            long pairsum = 0;
-            foreach (int value in numsum.Values)
-            {
-                pairsum += PairSum(value - 1);
-            }
+            pairsum += numsum[nums[i]] - 1;
 
-            for (int end = n - 1; end >= i; end--)
+            while (pairsum >= k && j <= i)
             {
-                if (pairsum >= k)
-                {
-                    res++;
-                }
-                else
-                {
-                    break;
-                }
-
-                pairsum -= PairSum(numsum[nums[end]] - 1);
-                numsum[nums[end]]--;
-                pairsum += PairSum(numsum[nums[end]] - 1);
+                res += (n - i);
+                numsum[nums[j]]--;
+                pairsum -= numsum[nums[j]];
+                j++;
             }
         }
 
