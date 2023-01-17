c67d780d25e4cd7c496bf383d39309d118c73cdc
eiji <eijiochiai8@yahoo.co.jp>
2023-01-17 13:25:54 +0900

    20230117

---
+++ 926. Flip String to Monotone Increasing/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MinFlipsMonoIncr(string s)
+    {
+        int ans = 0, num = 0;
+
+        for (int i = 0; i < s.Length; ++i)
+        {
+            if (s[i] == '0')
+            {
+                ans = Math.Min(num, ans + 1);
+            }
+            else
+            {
+                ++num;
+            }
+        }
+        return ans;
+    }
+}
