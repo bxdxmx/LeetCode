e661e21bf39246992c9b470d2bf48ab928e0267c
eiji <eijiochiai8@yahoo.co.jp>
2023-02-01 20:09:25 +0900

    20230201

---
+++ 1071. Greatest Common Divisor of Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿var s = new Solution();
+
+s.GcdOfStrings("ABCABC", "ABC");
+
+public class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        for (int n = Math.Min(str1.Length, str2.Length); n >= 1; n--)
+        {
+            if (str1.Length % n != 0 || str2.Length % n != 0)
+            {
+                continue;
+            }
+
+            string s1 = string.Concat(Enumerable.Repeat(str1[0..n], str1.Length / n).ToArray());
+            string s2 = string.Concat(Enumerable.Repeat(str1[0..n], str2.Length / n).ToArray());
+
+            if (str1 == s1 && str2 == s2)
+            {
+                return str1[0..n];
+            }
+        }
+
+        return string.Empty;
+    }
+}
---
+++ 1626. Best Team With No Conflicts/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    private class Member
+    {
+        public int Score { get; set; }
+        public int Age { get; set; }
+    }
+
+    public int BestTeamScore(int[] scores, int[] ages)
+    {
+        List<Member> members = new();
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            members.Add(new Member { Score = scores[i], Age = ages[i] });
+        }
+        members = members.OrderBy(m => m.Age).ThenBy(m => m.Score).ToList();
+
+        PriorityQueue<Member, int> q = new();
+
+
+
+
+
+
+    }
+}
